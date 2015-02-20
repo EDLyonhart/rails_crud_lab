79a5b22d745caca4cf6d326abcacd458d7bc54f9
EDLyonhart <EDLyonhart@gmail.com>
2015-02-19 19:26:45 -0800

    All basic requirements complete

---
+++ puppies/app/controllers/puppies_controller.rb
@@ -0,0 +1,48 @@
+class PuppiesController < ApplicationController
+  
+
+  def index
+    @puppies = Puppy.all
+  end
+
+  def show
+    @puppy = Puppy.find params[:id]
+  end
+
+  def new
+    @puppy = Puppy.new
+  end
+
+  def create
+    @puppy = Puppy.new puppy_params
+    if @puppy.save
+      redirect_to "/puppies"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @puppy = Puppy.find params[:id]
+  end
+
+  def update
+    @puppy = Puppy.find params[:id]
+    if @puppy.update_attributes puppy_params
+      redirect_to "/"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    puppy = Puppy.find params[:id]
+    puppy.destroy
+    redirect_to "/puppies"
+  end
+
+  private               #whitelisting // strong params
+    def puppy_params
+      params.require(:puppy).permit(:name, :age, :breed)
+    end
+end
--- puppies/config/routes.rb
+++ puppies/config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+
+root 'puppies#index'
+
+  #VERB '/rout', to: "controller#action"
+
+get '/puppies', to: "puppies#index"
+
+get '/puppies/new', to: "puppies#new"
+
+get '/puppies/:id', to: "puppies#show"
+
+get '/puppies/:id/edit', to: "puppies#edit"
+
+post '/puppies', to: "puppies#create"
+
+put '/puppies/:id', to: "puppies#update"
+
+delete '/puppies/:id', to: "puppies#destroy"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
